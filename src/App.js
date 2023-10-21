2462b716a2d63dee1fd6751a9e874b424d6fa3ff
k0nem <eamonkon@gmail.com>
2023-10-21 18:57:20 -0400

    Buttons layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from './components/Button';
-// import LargeButton from './components/LargeButton';
+import LargeButton from './components/LargeButton';
 import Output from './components/Output';
 import { useRef } from 'react';
 import { useState } from 'react';
@@ -23,23 +23,28 @@ function App() {
     <div className="outerContainer">
       <span className="display"><Output /></span>
       <div className="buttons">
-        <span className="button0"><Button value="0" /></span>
-        <span className="button."><Button value="." /></span>
-        <span className="buttonEnter"><Button value="Enter" /></span>    
-        <span className="button1"><Button value="1" /></span>
-        <span className="button2"><Button value="2" /></span>
-        <span className="button3"><Button value="3" /></span>
-        <span className="button4"><Button value="4" /></span>
-        <span className="button5"><Button value="5" /></span>
-        <span className="button6"><Button value="6" /></span>
-        <span className="button+"><Button value="+" /></span>
         <span className="button7"><Button value="7" /></span>
         <span className="button8"><Button value="8" /></span>
         <span className="button9"><Button value="9" /></span>
-        <span className="buttonC"><Button value="C" /></span>
         <span className="button/"><Button value="/" /></span>
+
+        <span className="button4"><Button value="4" /></span>
+        <span className="button5"><Button value="5" /></span>
+        <span className="button6"><Button value="6" /></span>
         <span className="button*"><Button value="*" /></span>
+
+        <span className="button1"><Button value="1" /></span>
+        <span className="button2"><Button value="2" /></span>
+        <span className="button3"><Button value="3" /></span>
         <span className="button-"><Button value="-" /></span>
+
+        <span className="button0"><Button value="0" /></span>
+        <span className="button."><LargeButton value="." /></span>
+        <span className="button+"><Button value="+" /></span>
+
+        <span className="buttonC"><LargeButton value="C" /></span>
+        <span className="buttonEnter"><LargeButton value="Enter" /></span>    
+
       </div>
     </div>
   );
